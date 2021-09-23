a578ec4f536a1797239ad783517c4419edef2b0c
EriWinckler <efwinckler@gmail.com>
2021-09-23 00:41:52 -0400

    added hand class

---
+++ src/com/company/uno/Hand.java
@@ -0,0 +1,23 @@
+package com.company.uno;
+
+import com.company.deck.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+    private List<Card> cards = new ArrayList<>();
+
+    public void addCard (Card card) {
+        cards.add(card);
+    }
+
+    public String displayHand() {
+        StringBuilder output = new StringBuilder();
+        for (Card card : cards) {
+            output.append(card.display()).append(" ");
+        }
+
+        return output.toString().trim();
+    }
+}
