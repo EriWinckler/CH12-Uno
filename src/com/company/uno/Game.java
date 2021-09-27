0d4bcd307e7b80183671e08d52f29eec9541873a
EriWinckler <efwinckler@gmail.com>
2021-09-27 02:28:29 -0400

    created startgame method

--- src/com/company/uno/Game.java
+++ src/com/company/uno/Game.java
@@ -10,7 +10,16 @@ public class Game {
     private int numberPlayers;
 
     //Initializing players array
-    public ArrayList<Player> players = new ArrayList<>();
+    private ArrayList<Player> players = new ArrayList<>();
+
+    private int playerCount = 0;
+    private boolean isActive = true;
+
+    //Round counter variable
+    private int round = 0;
+
+    //Creating variable to keep track of the current player
+    private Player currentPlayer;
 
     GameTable table = new GameTable();
 
@@ -19,7 +28,28 @@ public class Game {
         numberPlayers = Console.getInt(2, 10, "How many players are playing?", "Invalid number of players, try again");
 
         while(numberPlayers > players.size()) {
-            
+            players.add(createPlayer());
+        }
+
+        while(isActive) {
+            round();
+        }
+    }
+
+    public Player createPlayer() {
+        String name = Console.getString("Please enter player name", true);
+        Player player = new Player(name);
+        playerCount++;
+        return player;
+    }
+
+    public void round() {
+        //Round counter
+        round += 1;
+
+        //gameChecker
+        for (int i = 0; i < players.size(); i++) {
+            currentPlayer = players.get(i);
         }
     }
 }
