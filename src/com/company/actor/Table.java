7cb9849138655b9f4c0c257692719a5b1a14b331
EriWinckler <efwinckler@gmail.com>
2021-09-26 22:11:59 -0400

    created table class

--- src/com/company/actor/Table.java
+++ src/com/company/actor/Table.java
@@ -1,14 +1,27 @@
 package com.company.actor;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Table {
-     List<String> list = new ArrayList<String>();
+     private ArrayList<String> players = new ArrayList<String>();
     /*
     table
     hands
     displaytable
     remove player
      */
+
+     private ArrayList<String> table = new ArrayList<String>();
+
+     public ArrayList displayTable() {
+         return table;
+     }
+
+     public void removePlayer(String name) {
+          for(String player : players) {
+               if(player.equals(name)) {
+                    players.remove(name);
+               }
+          }
+     }
 }
